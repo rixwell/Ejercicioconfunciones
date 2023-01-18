044c34f95b0845f304d49c87ebe108627977d206
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-17 20:21:10 -0400

    Add files via upload

---
+++ Añobisiesto.cs
@@ -0,0 +1,20 @@
+﻿ Console.Write("Ingresa un año: ");
+        int anio = int.Parse(Console.ReadLine());
+
+        if
+            (EsBisiesto(anio))
+            Console.WriteLine("El año " + anio + " es bisiesto");
+        
+        else
+        
+            Console.WriteLine("El año " + anio + " no es bisiesto");
+
+        Console.ReadKey();
+
+    static bool EsBisiesto(int anio)
+    {
+        if (anio % 4 != 0) return false;
+        if (anio % 100 != 0) return true;
+        if (anio % 400 != 0) return false;
+        return true;
+    }
